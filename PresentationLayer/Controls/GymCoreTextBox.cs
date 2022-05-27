c735b530f1237b0fab3b9993a23dec1c20befcbb
FrancoNB <francobottini99@mi.unc.edu.ar>
2022-05-27 17:33:33 -0300

    Franco_SpecialControls_v1.0

---
+++ PresentationLayer/Controls/GymCoreTextBox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation.Controls
+{
+    public class GymCoreTextBox : TextBox
+    {
+        private readonly Color _foreColor = Color.FromArgb(139, 166, 145);
+        private readonly Color _backColor = Color.FromArgb(12, 19, 46);
+
+        public GymCoreTextBox()
+        {
+            BackColor = _backColor;
+            Size = new Size() { Height = 406 };
+            Font = new Font("Calibri", 12.0f);
+            BorderStyle = BorderStyle.FixedSingle;
+            ForeColor = _foreColor;
+        }
+
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+
+            if (e.KeyChar.Equals((char)Keys.Enter))
+            {
+                e.Handled = true;
+                SendKeys.Send("{TAB}");
+            }
+        }
+    }
+}
