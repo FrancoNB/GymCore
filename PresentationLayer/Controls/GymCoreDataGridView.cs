e2fcc48b2e417f19d44cb502341f5b5019fc7193
FrancoNB <francobottini99@mi.unc.edu.ar>
2022-05-27 18:26:07 -0300

    Franco_SpecialControls_v1.1

---
+++ PresentationLayer/Controls/GymCoreDataGridView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation.Controls
+{
+    public class GymCoreDataGridView : DataGridView
+    {
+
+    }
+}
