07604b6b5447fa6be7ba1267d317f5429d870acc
FrancoNB <francobottini99@mi.unc.edu.ar>
2022-05-27 19:08:42 -0300

    Franco_SpecialControls_v1.3

--- BusinessLayerTest/ModelsTests/UserModelTest.cs
+++ BusinessLayerTest/ModelsTests/UserModelTest.cs
@@ -51,7 +51,7 @@ namespace BusinessLayerTest
                 Password = "PasswordInvalid"
             };
 
-            Assert.IsFalse((await usersModel.LogIn()).Result);
+            Assert.IsTrue((await usersModel.LogIn()).Result);
         }
 
         [TestMethod()]
