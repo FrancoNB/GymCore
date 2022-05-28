15853a7bd44a222cfea679f63a2be3971495ad74
Fausto Lavezzari <faustolavezzari99@gmail.com>
2022-05-28 11:34:17 -0300

    Fausto_ClientsDA_v0.3

--- DataAccessLayer/Repositories/Interfaces/IClientsRepository.cs
+++ DataAccessLayer/Repositories/Interfaces/IClientsRepository.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using DataAccessLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessLayer.Repositories.Interfaces
+namespace DataAccessLayer.InterfaceRepositories
 {
-    internal class IClientsRepository
+    internal interface IClientsRepository : ICrudRepository<Clients>
     {
+
     }
 }
