010bf021fad04f3c022c3ac3554f09cd38d57d17
Fausto Lavezzari <faustolavezzari99@gmail.com>
2022-05-29 10:16:34 -0300

    Fausto_PackagesDA_v0.1

---
+++ DataAccessLayer/Entities/Packages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public class Packages
+    {
+        private int _idpackages;
+        private string _name;
+        private int _number_sessions;
+        private int _available_days;
+        private double _price;
+
+        public int IdPackages { get => _idpackages; set => _idpackages = value; }
+        public string Name { get => _name; set => _name = value; }
+        public int NumberSessions { get => _number_sessions; set => _number_sessions = value; }
+        public int AvailableDays { get => _available_days; set => _available_days = value; }
+        public double Price { get => _price; set => _price = value; }
+
+    }
+}
---
+++ DataAccessLayer/Repositories/PackagesRepository.cs
@@ -0,0 +1,81 @@
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.InterfaceRepositories;
+using DataAccessLayer.Support;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+
+namespace DataAccessLayer.Repositories.Interfaces
+{
+    public class PackagesRepository : RepositoryControler, IPackagesRepository
+    {
+        private readonly string insert;
+        private readonly string update;
+        private readonly string delete;
+        private readonly string selectAll;
+
+        public PackagesRepository()
+        {
+            this.insert = "INSERT INTO Packages (Name, NumberSessions, AvailableDays, Price) VALUES (@name, @number_sessions, @available_days, @price)";
+            this.update = "UPDATE Packages SET Name = @name, NumberSessions = @number_sessions, AvailableDays = @available_days, Price = @price";
+            this.delete = "DELETE FROM Packages WHERE IdPackages = @idPackages";
+            this.selectAll = "SELECT * FROM Packages";
+        }
+
+        public async Task<int> Insert(Packages entity)
+        {
+            parameters = new List<MySqlParameter> {
+                new MySqlParameter("@name", entity.Name),
+                new MySqlParameter("@number_sessions", entity.NumberSessions),
+                new MySqlParameter("@available_days", entity.AvailableDays),
+                new MySqlParameter("@price", entity.Price)
+            };
+            return await ExecuteNonQueryAsync(insert);
+        }
+
+        public async Task<int> Update(Packages entity)
+        {
+            parameters = new List<MySqlParameter>
+            {
+                new MySqlParameter("@name", entity.Name),
+                new MySqlParameter("@number_sessions", entity.NumberSessions),
+                new MySqlParameter("@available_days", entity.AvailableDays),
+                new MySqlParameter("@price", entity.Price)
+            };
+            return await ExecuteNonQueryAsync(update);
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            parameters = new List<MySqlParameter>
+            {
+                new MySqlParameter("@idPackages", id)
+            };
+            return await ExecuteNonQueryAsync(delete);
+        }
+
+        public async Task<IEnumerable<Packages>> GetAll()
+        {
+            using (var table = await ExecuteReaderAsync(selectAll))
+            {
+                var list = new List<Packages>();
+
+                foreach (DataRow row in table.Rows)
+                {
+                    list.Add(new Packages()
+                    {
+                        IdPackages = Convert.ToInt32(row["IdExercises"]),
+                        Name = row["Name"].ToString(),
+                        NumberSessions = Convert.ToInt32(row["NumberSessions"]),
+                        AvailableDays = Convert.ToInt32(row["AvailableDays"]),
+                        Price = Convert.ToDouble(row["Price"])                    
+                    });
+                }
+                return list;
+            }
+        }
+    }
+}
