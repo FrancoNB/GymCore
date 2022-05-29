14ebd64374f9a36043649350e3dea8c998ddc7eb
Fausto Lavezzari <faustolavezzari99@gmail.com>
2022-05-29 11:21:49 -0300

    PackagesDA_v1.0
    * Fausto_PackagesDA_v0.1
    
    * Fausto_PackagesDA_v0.2

---
+++ DataAccessLayer/Entities/Packages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public class Packages
+    {
+        private int _idPackages;
+        private string _name;
+        private int _numberSessions;
+        private int _availableDays;
+        private double _price;
+
+        public int IdPackages { get => _idPackages; set => _idPackages = value; }
+        public string Name { get => _name; set => _name = value; }
+        public int NumberSessions { get => _numberSessions; set => _numberSessions = value; }
+        public int AvailableDays { get => _availableDays; set => _availableDays = value; }
+        public double Price { get => _price; set => _price = value; }
+
+    }
+}
