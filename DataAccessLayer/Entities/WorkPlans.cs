ae840955e15f40ba832c5a97da414ce2954e231a
Fausto Lavezzari <faustolavezzari99@gmail.com>
2022-05-29 10:48:52 -0300

    WorkPlansDA_v0.1

---
+++ DataAccessLayer/Entities/WorkPlans.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public class WorkPlans
+    {
+        private int _idworkplans;
+        private string _name;
+        private string _category;
+
+        public int IdWorkPlans { get => _idworkplans; set => _idworkplans = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Category { get => _category; set => _category = value; }
+    }
+}
