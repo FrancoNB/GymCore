976925d48017f16d6c5e85f51e4ea4a9757bbcc6
Fausto Lavezzari <faustolavezzari99@gmail.com>
2022-05-28 21:08:27 -0300

    Fausto_ClientsDA_v1.1.1

--- DataAccessLayer/Repositories/ClientsRepository.cs
+++ DataAccessLayer/Repositories/ClientsRepository.cs
@@ -21,7 +21,7 @@ namespace DataAccessLayer.Repositories.Interfaces
         {
             this.insert = "INSERT INTO Clients (RegisterDate, Name, Surname, Locality, Address, Phone, Mail, Observations, State) VALUES (@registerDate, @name, @surname, @locality, @address, @phone, @mail, @observations, @state)".
             this.update = "UPDATE Clients SET RegisterDate = @registerDate, Name = @name, Surname = @surname, Locality = @locality, Address = @addres, Phone = @phone, Mail = @mail, State = @state WHERE IdClients = @idClients";
-            this.delete = "DELELTE FROM Clients WHERE IdClients = @idClients";
+            this.delete = "DELETE FROM Clients WHERE IdClients = @idClients";
             this.selectAll = "SELECT * FROM Clients";
         }
 
