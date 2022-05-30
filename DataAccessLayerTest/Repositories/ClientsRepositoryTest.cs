da351b6deaa6c7870513b488205fc5ef6c7dcd18
FrancoNB <francobottini99@mi.unc.edu.ar>
2022-05-30 08:47:30 -0300

    Franco_DataAccessLayerTest_v0.1

---
+++ DataAccessLayerTest/Repositories/ClientsRepositoryTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DataAccessLayerTest
+{
+    [TestClass]
+    public class ClientsRepositoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
