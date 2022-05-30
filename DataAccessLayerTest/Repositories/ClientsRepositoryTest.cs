e642d4b8c3b80de4eceeb9ccb792086ec63f6493
Franco Nicolas Bottini <101523922+FrancoNB@users.noreply.github.com>
2022-05-30 11:21:03 -0300

    DataAccessLayerTest_v1.0
    * Franco_DataAccessLayerTest_v0.1
    
    * Franco_DataLayerTest_v0.2
    
    * Franco_DataLayerTest_v0.3

---
+++ DataAccessLayerTest/Repositories/ClientsRepositoryTest.cs
@@ -0,0 +1,85 @@
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.InterfaceRepositories;
+using DataAccessLayer.Repositories.Interfaces;
+using DataAccessLayer.Support;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace DataAccessLayerTest
+{
+    [TestClass]
+    public class ClientsRepositoryTest
+    {
+        private IClientsRepository repository;
+        private Clients entity;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            repository = new ClientsRepository();
+        }
+
+        [TestMethod]
+        public async Task Insert_ValidTest()
+        {
+            entity = new Clients()
+            {
+                RegisterDate = DateTime.Now,
+                Name = "Walter",        
+                Surname = "Lopez",
+                Locality = "Jesus Maria",
+                Address = "Sarmiento 205",
+                Phone = "649151616161",
+                Mail = "asdadsada",
+                Observations = "-"
+            };
+
+            try
+            {
+                RepositoryConnection.BeginTransaction();
+
+                Assert.AreEqual(1, await repository.Insert(entity));
+            } 
+            finally
+            {
+                RepositoryConnection.RollBack();
+            }
+        }
+
+        [TestMethod]
+        public async Task Insert_InvalidTest()
+        {
+            entity = new Clients()
+            {
+                RegisterDate = DateTime.Now,
+                Locality = "Jesus Maria",
+                Address = "Sarmiento 205",
+                Phone = "649151616161",
+                Mail = "asdadsada",
+                Observations = "-"
+            };
+
+            try
+            {
+                RepositoryConnection.BeginTransaction();
+
+                var ex = await Assert.ThrowsExceptionAsync<RepositoryException>(() => repository.Insert(entity));
+
+                Assert.AreEqual(1048, ex.Code);
+            }
+            finally
+            {
+                RepositoryConnection.RollBack();
+            }
+        }
+
+        [TestMethod]
+        public async Task GetAll_Test()
+        {
+            CollectionAssert.AllItemsAreInstancesOfType((List<Clients>)await repository.GetAll(), typeof(Clients));
+        }
+    }
+}
