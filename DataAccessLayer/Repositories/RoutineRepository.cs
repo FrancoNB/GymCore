d7e38ca09ab0092efaef49abfd42e2aa5acc5648
Fausto Lavezzari <faustolavezzari99@gmail.com>
2022-05-30 08:51:59 -0300

    RoutineDA_v1.0
    * Fausto_Routine_v0.1
    
    * Fausto_Routine_v0.2
    
    * Update IRoutineRepository.cs
    
    * Update DataAccessLayer.csproj
    
    * Update DataAccessLayer.csproj
    
    Co-authored-by: FrancoNB <francobottini99@mi.unc.edu.ar>

---
+++ DataAccessLayer/Repositories/RoutineRepository.cs
@@ -0,0 +1,86 @@
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.InterfaceRepositories;
+using DataAccessLayer.Support;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+
+namespace DataAccessLayer.Repositories.Interfaces
+{
+    public class RoutineRepository : RepositoryControler, IRoutineRepository
+    {
+
+        private readonly string insert;
+        private readonly string update;
+        private readonly string delete;
+        private readonly string selectAll;
+
+        public RoutineRepository()
+        {
+            this.insert = "INSERT INTO Routine (StartDate, EndDate, State, IdClients, IdWorksPlans) VALUES (@startDate, @endDate, @state, @idClients, @idWorksPlans)";
+            this.update = "UPDATE Routine SET StartDate = @startDate, EndDate = @endDate, State = @state, IdClients = @idClients, IdWorksPlans = @idWorksPlans WHERE IdRoutine = @idRoutine";
+            this.delete = "DELETE FROM Routne WHERE IdRoutine = @idRoutine";
+            this.selectAll = "SELECT * FROM Routine";
+        }
+
+        public async Task<int> Insert(Routine entity)
+        {
+            parameters = new List<MySqlParameter> {
+                new MySqlParameter("@startDate", entity.StartDate),
+                new MySqlParameter("@endDate",entity.EndDate),
+                new MySqlParameter("@state",entity.State),
+                new MySqlParameter("@idClients",entity.IdClients),
+                new MySqlParameter("@idWorksPlans ",entity.IDWorkPlans)
+            };
+            return await ExecuteNonQueryAsync(insert);
+        }
+
+        public async Task<int> Update(Routine entity)
+        {
+            parameters = new List<MySqlParameter>
+            {
+                new MySqlParameter("@idRoutine",entity.IdRoutine),
+                new MySqlParameter("@startDate", entity.StartDate),
+                new MySqlParameter("@endDate",entity.EndDate),
+                new MySqlParameter("@state",entity.State),
+                new MySqlParameter("@idClients",entity.IdClients),
+                new MySqlParameter("@idWorksPlans ",entity.IDWorkPlans)
+            };
+            return await ExecuteNonQueryAsync(update);
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            parameters = new List<MySqlParameter>
+            {
+                new MySqlParameter("@idRoutine", id)
+            };
+            return await ExecuteNonQueryAsync(delete);
+        }
+
+        public async Task<IEnumerable<Routine>> GetAll()
+        {
+            using (var table = await ExecuteReaderAsync(selectAll))
+            {
+                var list = new List<Routine>();
+
+                foreach (DataRow row in table.Rows)
+                {
+                    list.Add(new Routine()
+                    {
+                        IdRoutine = Convert.ToInt32(row["IdRoutine"]),
+                        StartDate = Convert.ToDateTime(row["StartDate"]),
+                        EndDate = Convert.ToDateTime(row["EndDate"]),
+                        State = row["State"].ToString(),
+                        IdClients = Convert.ToInt32(row["IdClients"]),
+                        IDWorkPlans = Convert.ToInt32(row["IdWorkPlans"])
+                    });
+                }
+                return list;
+            }
+        }
+    }
+}
