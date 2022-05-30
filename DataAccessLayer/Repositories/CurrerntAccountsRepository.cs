a2c160750e799a15892fe023671548645a34eb2c
Fausto Lavezzari <faustolavezzari99@gmail.com>
2022-05-30 02:43:28 -0300

    Fausto_CurrentAccounts_v0.2

--- DataAccessLayer/Repositories/CurrerntAccountsRepository.cs
+++ DataAccessLayer/Repositories/CurrerntAccountsRepository.cs
@@ -1,12 +1,95 @@
-﻿using System;
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.InterfaceRepositories;
+using DataAccessLayer.Support;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Data;
 using System.Threading.Tasks;
 
-namespace DataAccessLayer.Repositories
+
+namespace DataAccessLayer.Repositories.Interfaces
 {
-    internal class CurrerntAccountsRepository
+    public class CurrentAccountsRepository : RepositoryControler, ICurrentAccountsRepository
     {
+        private readonly string insert;
+        private readonly string update;
+        private readonly string delete;
+        private readonly string selectAll;
+
+        public CurrentAccountsRepository()
+        {
+            this.insert = "INSERT INTO CurrentAccounts (TicketCode, Date, Credit, Debit, Balance, Detail, IdClients) VALUES (@ticketCode, @date, @credit, @debit, @balance, @detail, @idClients)";
+
+            this.update = "UPDATE CurrentAccounts SET TicketCode = @ticketCode, Date = @date, Credit = @credit, Debit = @debit, Balance = @balance, Detail = @detail, "
+                        + "IdClients = @idClients WHERE IdCurrentAccounts = @idCurrentAccounts";
+
+            this.delete = "DELETE FROM CurrentAccounts WHERE IdCurrentAccounts = @idCurrentAccounts";
+
+            this.selectAll = "SELECT * FROM CurrentAccounts";
+        }
+
+        public async Task<int> Insert(CurrentAccounts entity)
+        {
+            parameters = new List<MySqlParameter> {
+                new MySqlParameter("TicketCode", entity.TicketCode),
+                new MySqlParameter("Date",entity.Date),
+                new MySqlParameter("Credit",entity.Credit),
+                new MySqlParameter("Debit",entity.Debit),
+                new MySqlParameter("Balance",entity.Balance),
+                new MySqlParameter("Detail",entity.Detail),
+                new MySqlParameter("IdClients",entity.IdClients)
+            };
+            return await ExecuteNonQueryAsync(insert);
+        }
+
+        public async Task<int> Update(CurrentAccounts entity)
+        {
+            parameters = new List<MySqlParameter>
+            {
+                new MySqlParameter("IdCurrentAccounts",entity.IdCurrentAccounts),
+                new MySqlParameter("TicketCode", entity.TicketCode),
+                new MySqlParameter("Date",entity.Date),
+                new MySqlParameter("Credit",entity.Credit),
+                new MySqlParameter("Debit",entity.Debit),
+                new MySqlParameter("Balance",entity.Balance),
+                new MySqlParameter("Detail",entity.Detail),
+                new MySqlParameter("IdClients",entity.IdClients)
+            };
+            return await ExecuteNonQueryAsync(update);
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            parameters = new List<MySqlParameter>
+            {
+                new MySqlParameter("@idCurrentAccounts", id)
+            };
+            return await ExecuteNonQueryAsync(delete);
+        }
+
+        public async Task<IEnumerable<CurrentAccounts>> GetAll()
+        {
+            using (var table = await ExecuteReaderAsync(selectAll))
+            {
+                var list = new List<CurrentAccounts>();
+
+                foreach (DataRow row in table.Rows)
+                {
+                    list.Add(new CurrentAccounts()
+                    {
+                        IdCurrentAccounts = Convert.ToInt32(row["IdCurrentAccounts"]),
+                        TicketCode = row["TicketCode"].ToString(),
+                        Date = Convert.ToDateTime(row["Date"]),
+                        Credit = Convert.ToDouble(row["Credit"]),
+                        Debit = Convert.ToDouble(row["Debit"]),
+                        Balance = Convert.ToDouble(row["Balance"]),
+                        Detail = row["Detail"].ToString(),
+                        IdClients = Convert.ToInt32(row["IdClients"])
+                    });
+                }
+                return list;
+            }
+        }
     }
 }
