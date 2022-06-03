e455fcc3ed1c331e262da8458b8b959824ec4e64
FrancoNB <francobottini99@mi.unc.edu.ar>
2022-06-03 08:49:30 -0300

    Franco_ManagementSubscriptionsUI_v0.1

---
+++ PresentationLayer/Forms/Management/frmManagementSubscriptions.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Presentation.Forms.Management
+{
+    partial class frmManagementSubscriptions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmManagementSubscriptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(16)))), ((int)(((byte)(23)))));
+            this.ClientSize = new System.Drawing.Size(1024, 600);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmManagementSubscriptions";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmManagementSubscriptions";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
